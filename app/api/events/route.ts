e5a1f5cb61fb65a848884d6744c48e45cac4a9c3
unknown <robinaanne93@gmail.com>
2026-05-30 23:07:39 +0800

    Configure GET request for events

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -41,3 +41,15 @@ export async function POST(req:NextRequest) {
         return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message: 'Unknown' })
     }
 }
+
+export async function GET(){
+    try {
+        await connectToDatabase();
+
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: 'Events fetched successfully', events }, { status: 200 });
+    } catch (e) {
+        return NextResponse.json({ message: 'Event fetching failed', error: e }, { status: 500 });
+    }
+}
